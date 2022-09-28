729a0939e29a9050be8e0122874781608cb498fa
Romero Ezequiel <romerorezequiel@gmail.com>
2022-09-27 22:20:23 -0300

    Cree el controler para la persona

---
+++ CRUD/CRUD/Controllers/PersonaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRUD.Controllers
+{
+    public class PersonaController : Controller
+    {
+        // GET: Persona
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
